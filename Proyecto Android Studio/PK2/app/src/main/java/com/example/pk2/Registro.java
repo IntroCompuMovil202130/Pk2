d5c9d29cbe1f2cec5760dc97def6c25d9dc3c577
Alejandro <d.alejandro198@gmail.com>
2021-08-14 16:55:53 -0500

    primeras interfaces

---
+++ Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/Registro.java
@@ -0,0 +1,16 @@
+package com.example.pk2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.moraitoMelo));
+
+    }
+}
