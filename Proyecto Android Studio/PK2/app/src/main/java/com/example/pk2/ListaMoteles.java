57281df3e373eb7aa631dcb694e103c65fd25e29
Estiben <bestiben.giraldol@javeriana.edu.co>
2021-08-29 01:11:38 -0500

    Pantalla Lista de Habitaciones

--- Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/ListaMoteles.java
+++ Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/ListaMoteles.java
@@ -1,6 +1,7 @@
 package com.example.pk2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.ImageButton;
 
 public class ListaMoteles extends AppCompatActivity {
     ImageButton bPerfil;
+    CardView selecMotel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,7 @@ public class ListaMoteles extends AppCompatActivity {
 
         //inflate
         bPerfil = findViewById(R.id.botonPerifl);
+        selecMotel = findViewById(R.id.cardView3);
 
         bPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +28,14 @@ public class ListaMoteles extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        selecMotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), activity_lista_habitaciones.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
