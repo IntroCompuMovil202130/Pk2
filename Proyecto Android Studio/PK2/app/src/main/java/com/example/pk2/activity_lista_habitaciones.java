0a33636a54f52b03f5229b9b9663ffaef68c471f
Estiben <bestiben.giraldol@javeriana.edu.co>
2021-08-29 13:01:13 -0500

    Pantalla Lista de Habitaciones e infoHabitaciones sin fotos

--- Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/activity_lista_habitaciones.java
+++ Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/activity_lista_habitaciones.java
@@ -1,15 +1,32 @@
 package com.example.pk2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_lista_habitaciones extends AppCompatActivity {
 
+    CardView selecHabitacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_habitaciones);
+
+        //inflate
+        selecHabitacion = findViewById(R.id.habSencilla);
+
+
+        selecHabitacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), activity_descripcion_habitacion.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
