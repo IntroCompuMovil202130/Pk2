63461cb24b82a3a31b57619423cc781fea908465
Antolinez17 <48690314+Az0917@users.noreply.github.com>
2021-08-29 18:55:12 -0500

    Pantalla de mapa

--- Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/activity_lista_habitaciones.java
+++ Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/activity_lista_habitaciones.java
@@ -6,10 +6,12 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class activity_lista_habitaciones extends AppCompatActivity {
 
     CardView selecHabitacion;
+    Button Bvamos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +20,14 @@ public class activity_lista_habitaciones extends AppCompatActivity {
 
         //inflate
         selecHabitacion = findViewById(R.id.habSencilla);
-
-
+        Bvamos = findViewById(R.id.botonVamos);
+        Bvamos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intVAmos = new Intent(v.getContext(),MapsActivity.class);
+                startActivity(intVAmos);
+            }
+        });
         selecHabitacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
