a737e67bb0024d145447e8f62627be7bd5c454ff
Estiben <bestiben.giraldol@javeriana.edu.co>
2021-10-08 20:04:47 -0500

    Pantallas Crear Mote, Modificar motel y Crear Habitacion

---
+++ Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/crear_motel.java
@@ -0,0 +1,14 @@
+package com.example.pk2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class crear_motel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_motel);
+    }
+}
---
+++ Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/editar_informacion_motel.java
@@ -0,0 +1,14 @@
+package com.example.pk2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editar_informacion_motel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_informacion_motel);
+    }
+}
