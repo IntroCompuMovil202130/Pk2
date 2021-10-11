978b875520d678e56e701e761e7e555fabc82085
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2021-10-11 16:18:01 -0500

    Update AdmLogActivity.java

--- Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/AdmLogActivity.java
+++ Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/AdmLogActivity.java
@@ -2,13 +2,25 @@ package com.example.pk2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdmLogActivity extends AppCompatActivity {
+    Button btnAgHab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adm_log);
+        btnAgHab = findViewById(R.id.btnAgHab);
+        btnAgHab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intCrearHab = new Intent(v.getContext(),crear_habitacon.class);
+                startActivity(intCrearHab);
+            }
+        });
     }
 }
