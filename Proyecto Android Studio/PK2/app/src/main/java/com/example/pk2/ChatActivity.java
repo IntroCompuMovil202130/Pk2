0ae521ff3a91cb52dd1baa0badd0018a7247510d
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2021-11-23 01:52:04 -0500

    Update ChatActivity.java

--- Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/ChatActivity.java
+++ Proyecto Android Studio/PK2/app/src/main/java/com/example/pk2/ChatActivity.java
@@ -3,16 +3,24 @@ package com.example.pk2;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.example.pk2.model.Chat;
+import com.example.pk2.model.Mensaje_Texto;
 import com.example.pk2.model.Usuario;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +28,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -32,18 +43,55 @@ public class ChatActivity extends AppCompatActivity {
     static final String PATH_USERS = "users/";
     static final String PATH_DUENO = "dueno/";
 
+    private AdaptadorChat a_Chat;
+
+    private RecyclerView r_ListMensajes;
+
+    private List<Mensaje_Texto> testList;
+
+    Usuario mySender, myReciever;
+
+    Intent currIntent;
+
+    boolean esDueno;
+
+    TextInputEditText e_Text;
+
+    ImageButton b_Send;
+
 
     @SuppressLint("WrongConstant")
     @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
-        getWindow().getDecorView().getWindowInsetsController().setSystemBarsAppearance(0x00000008, 0x00000008);
         //inflate base de datos y autenticacion
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
+        currIntent = getIntent();
+        esDueno = currIntent.getBooleanExtra("esDueno", false);
+        getUsers();
+        b_Send = findViewById(R.id.botonEnviarChat);
+        e_Text = findViewById(R.id.messageTextChat);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+        getWindow().getDecorView().getWindowInsetsController().setSystemBarsAppearance(0x00000008, 0x00000008);
+
+        b_Send.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(e_Text.getText().toString().trim()))
+                {
+                    Date date = new Date();
+                    Mensaje_Texto nuevoMensaje = new Mensaje_Texto(e_Text.getText().toString(), mySender, date.getTime());
+                    sendMessage(nuevoMensaje);
+                    e_Text.setText("");
+                }
+            }
+        });
+
     }
     /*inflate del menu y manejo de las opciones de este*/
     @Override
@@ -76,7 +124,7 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     private void comprobarD(Menu menu){
-        myRefU = database.getReference(PATH_DUENO);
+        myRefD = database.getReference(PATH_DUENO);
         myRefD.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -95,6 +143,7 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
+
     }
     private void comprobarU(Menu menu)
     {
@@ -142,6 +191,188 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
     }
+    private void sendMessage(Mensaje_Texto nuevoMensaje)
+    {
+        myRefU = database.getReference(PATH_USERS);
+        a_Chat.addMessage(nuevoMensaje);
+        if (esDueno)
+        {
+
+            myRefU.orderByChild("id").equalTo(myReciever.getId()).addListenerForSingleValueEvent(new ValueEventListener()
+            {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot)
+                {
+                    for (DataSnapshot child : snapshot.getChildren()){
+                        Usuario currUser = child.getValue(Usuario.class);
+                        if(!currUser.doesChatExists(mySender.getId()))
+                        {
+                            Chat myNewChat = new Chat(mySender.getId());
+                            myReciever.agregarChat(myNewChat);
+                        }
+                        myReciever.agregarMensaje(nuevoMensaje, mySender.getId());
+                        myRefU.child(myReciever.getId()).setValue(myReciever);
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+        else
+        {
+
+            myRefU.orderByChild("id").equalTo(mySender.getId()).addListenerForSingleValueEvent(new ValueEventListener()
+            {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot)
+                {
+                    for (DataSnapshot child : snapshot.getChildren())
+                    {
+                        Usuario currUser = child.getValue(Usuario.class);
+                        if(!currUser.doesChatExists(myReciever.getId()))
+                        {
+                            Chat myNewChat = new Chat(myReciever.getId());
+                            mySender.agregarChat(myNewChat);
+                            myRefU.child(mySender.getId()).child("/listChats/").child("0/").setValue(myNewChat);
+
+                        }
+                        mySender.agregarMensaje(nuevoMensaje, myReciever.getId());
+                        
+                        myRefU.child(mySender.getId()).child("/listChats/").child("noMensajes/").setValue(nuevoMensaje);
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+    }
+    private void displayList()
+    {
+
+        r_ListMensajes = findViewById(R.id.chat_recycler_view);
+        if (esDueno)
+        {
+            //Inflar reciclador y crear adaptador.
+            a_Chat = new AdaptadorChat(this, myReciever.getMessageList(mySender.getId()));
+            r_ListMensajes.setLayoutManager(new LinearLayoutManager(this));
+            r_ListMensajes.setAdapter(a_Chat);
+
+        }
+        else
+        {
+            //Inflar reciclador y crear adaptador.
+            a_Chat = new AdaptadorChat(this, mySender.getMessageList(myReciever.getId()));
+            r_ListMensajes.setLayoutManager(new LinearLayoutManager(this));
+            r_ListMensajes.setAdapter(a_Chat);
+        }
+    }
+
+    private void getUsers()
+    {
+        myRefU = database.getReference(PATH_USERS);
+        myRefD = database.getReference(PATH_DUENO);
+        Log.d("TEST_A",currIntent.getStringExtra("idReciever"));
+        if (esDueno)
+        {
+            myRefD.addListenerForSingleValueEvent(new ValueEventListener()
+            {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for(DataSnapshot child : snapshot.getChildren())
+                    {
+                        Usuario buscado = child.getValue(Usuario.class);
+                        FirebaseUser logeado = mAuth.getCurrentUser();
+                        if(buscado.getId().equals(logeado.getUid()))
+                        {
+                            mySender = buscado;
+                        }
+                    }
+                    myRefU.addListenerForSingleValueEvent(new ValueEventListener()
+                    {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for(DataSnapshot child : snapshot.getChildren())
+                            {
+                                Usuario buscado = child.getValue(Usuario.class);
+                                if(buscado.getId().equals(currIntent.getStringExtra("idReciever")))
+                                {
+                                    myReciever = buscado;
+                                }
+                            }
+                            displayList();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+        else
+        {
+            myRefU.addListenerForSingleValueEvent(new ValueEventListener()
+            {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for(DataSnapshot child : snapshot.getChildren())
+                    {
+                        Usuario buscado = child.getValue(Usuario.class);
+                        FirebaseUser logeado = mAuth.getCurrentUser();
+                        if(buscado.getId().equals(logeado.getUid()))
+                        {
+                            mySender = buscado;
+                        }
+                    }
+                    myRefD.addListenerForSingleValueEvent(new ValueEventListener()
+                    {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for(DataSnapshot child : snapshot.getChildren())
+                            {
+                                Usuario buscado = child.getValue(Usuario.class);
+
+                                Log.d("TEST_A", buscado.getId());
+                                if(buscado.getId().equals(currIntent.getStringExtra("idReciever")))
+                                {
+                                    myReciever = buscado;
+                                }
+                            }
+                            displayList();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+
+
+    }
     private void ubicacionOff()
     {
         myRefU = database.getReference(PATH_USERS);
@@ -165,4 +396,6 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
